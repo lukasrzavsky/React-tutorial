a8cf8c073801e788f578584667078360954c51a6
rzavsky <rzavsky@RZAVSKY>
2019-08-16 10:34:27 +0200

    propTypes added, reference to element added(focus to some element)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,33 +5,46 @@ import withClass from '../../../hoc/withClass';
 import PropTypes from 'prop-types';
 
 //Hook
-// class Person extends Component {
-//     render() {
-//         console.log('[Person.js rendering...]')
-//         return (
-//             <div className={classes.Person} >
-//                 <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
-//                 <p>{this.props.children}</p>
-//                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-//             </div>
-//         )
+class Person extends Component {
 
-//     }
-// }
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
 
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+    render() {
+        console.log('[Person.js rendering...]')
+        return (
+            <div className={classes.Person} >
+                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input 
+                // ref={(inputEl) => {this.inputElement = inputEl}} 
+                ref={this.inputElementRef}
+                type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        )
 
-const Person = ({ name, age, children, changed, clicked, id }) => {
-    console.log('[Person.js rendering...]')
-    return (
-        // <div className={classes.Person} >
-        <Fragment>
-            <p onClick={() => clicked(id)}>I am {name} and I am {age} years old</p>
-            <p>{children}</p>
-            <input type="text" onChange={(event) => changed(event, id)} value={name} />
-        {/* </div> */}
-        </Fragment>
-    )
+    }
 }
+
+
+// const Person = ({ name, age, children, changed, clicked, id }) => {
+//     console.log('[Person.js rendering...]')
+//     return (
+//         // <div className={classes.Person} >
+//         <Fragment>
+//             <p onClick={() => clicked(id)}>I am {name} and I am {age} years old</p>
+//             <p>{children}</p>
+//             <input type="text" onChange={(event) => changed(event, id)} value={name} />
+//         {/* </div> */}
+//         </Fragment>
+//     )
+// }
 
 Person.propTypes = {
     click: PropTypes.func,
